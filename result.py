b7f42bac8efd35d411ad84e575a97a05d0578b69
zyzo <danghaian168@gmail.com>
2015-11-30 12:23:23 +0100

    Add result.py to generate an analysis of multiple graphs with multiple number of partitions

---
+++ result.py
@@ -0,0 +1,18 @@
+from GreedyPartitioning import GreedyPartitioning
+import glob
+import input
+import eval
+print glob.glob("./graph/*")
+data = open("graph_link.txt", 'r')
+
+TEST_PARTITIONS = [2,4,6,8]
+
+for partition in TEST_PARTITIONS:
+    print "CAS ", partition, " PARTITIONS"
+    for line in glob.glob("./graph/*"):
+        print "Graph : ", line
+        g = GreedyPartitioning(line, partition)
+        g2 = input.read_local(line)
+        print "Cut : ", eval.cut(g2[2], g.get_partitions())
+        print "Ratio : ", eval.ratio(g2[2], g.get_partitions())
+
